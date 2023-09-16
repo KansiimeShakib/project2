2d69541560bc39e43aab6aed0a19488b0b1f8bcd
Kansiime Shakib <shakibkansiime97@gmail.com>
2023-09-16 14:16:48 +0300

    adding 1

---
+++ app.js
@@ -0,0 +1,17 @@
+let btnEl = document.getElementById("btn-el")
+let zeroEl = document.getElementById("zero-el")
+// console.log(zeroEl)
+
+zeroEl = 0;
+// console.log(zeroEl + 1)
+
+btnEl.addEventListener("click", cart);
+
+function cart(){
+  zeroEl = 0;
+  for (i=0; i<zeroEl.length; i++){
+    zeroEl = zeroEl + 1
+  }
+  // zeroEl.textContent = zeroEl + 1
+  console.log(zeroEl[i])
+}
