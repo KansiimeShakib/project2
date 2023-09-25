53de52c4b8560d16ecb19866d2225106828e0346
Kansiime Shakib <shakibkansiime97@gmail.com>
2023-09-25 15:45:38 +0300

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
 let btnEl = document.getElementById("btn-el")
-let zeroEl = document.getElementById("zero-el")
-// console.log(zeroEl)
+let zeroEl = document.getElementById("zero-el");
+let zero2El = document.getElementById("zero2-el");
+let delEl = document.getElementById("del-el");
+let subEl = document.getElementById("sub-el");
+let addEl = document.getElementById("add-el");
+let exEl = document.getElementById("ex-el");
+let tryingEl = document.getElementById("trying-el");
+let cartCollector = document.getElementById("cart-collector");
 
-let count = 0;
-// console.log(zeroEl + 1)
+let lex = [
+  {
+    image: "image/shoe 2.jpg",
+    tittle: "Snikers",
+    price: 35,
+  },
+]
+ 
+const copy = [...new setInterval(lex.map((item)=>
+  {return item}))]
 
-btnEl.addEventListener("click",cart)
+  let i = "";
 
-function cart(){
-  zeroEl.innerHTML = count++
-}
+// exEl.addEventListener("click",()=>{
+//   return cartCollector
+//   console.log()
+// })
+
+
+let innert = 0;
+
+btnEl.addEventListener("click",()=>{
+  zeroEl.innerHTML = innert++
+});
+
+
+addEl.addEventListener("click",()=>{
+  zero2El.innerHTML = innert++
+})
+
+subEl.addEventListener("click",()=>{
+  innert = innert++
+  zero2El.innerHTML = innert--
+})
+
+
+delEl.addEventListener("click", ()=>{
+  tryingEl.remove();
+})
+
+
+
+
+
+
+
+
+
+
+
